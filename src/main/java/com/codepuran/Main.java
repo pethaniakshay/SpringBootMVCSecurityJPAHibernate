561730a4e32c7938d6f708738d912371696ddb7d
Akshay Pethani <patelaksh412@gmail.com>
2018-01-07 00:41:37 +0530

    Initial Commit

---
+++ src/main/java/com/codepuran/Main.java
@@ -0,0 +1,22 @@
+package com.codepuran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class Main extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Main.class);
+  }
+
+  public static void main(String args[]) {
+    SpringApplication.run(Main.class, args);
+  }
+}
+
